38e53be77244c5944f3ce24e3b4af795fcbc0cc9
Mykhailo Iartysh <mykhailo_iartysh@epam.com>
2016-11-11 17:51:39 +0200

    first commit

---
+++ simpleAppExpress/public/js/script.js
@@ -0,0 +1,54 @@
+// Add item to list on click
+$('.add-item').click(function(event) {
+  event.preventDefault();
+
+  if ($('.add-input').val().trim() == '') {
+    $('.add-input').val('');
+    return;
+  }
+
+  $.ajax({
+    method: 'POST',
+    url: '/add-item',
+    data: {item: $('.add-input').val()}
+  })
+    .done( function(msg) {
+      $('.add-input').val('');
+      $('.all-items').html(generateList(msg.items));
+  });
+})
+
+// Delete all items from list
+$('.del-items').click(function(event) {
+  event.preventDefault();
+
+  $.ajax({
+    method: 'DELETE',
+    url: '/delete-items',
+  })
+    .done(function(msg) {
+      $('.all-items').html(generateList(msg.items));
+  });
+})
+
+// Get all items when page is loaded
+$(function() {
+  $.ajax({
+    method: 'GET',
+    url: '/get-items',
+  })
+    .done(function( msg ) {
+      $('.all-items').html(generateList(msg.items));
+  });
+});
+
+
+function generateList(itemData) {
+  console.log(itemData)
+
+  var itemList = itemData.reduce(function(prevList, current){
+    return prevList + '<li>' + current + '</li>'
+  }, '')
+  console.log(itemList);
+  return '<ul>' + itemList + '</ul>'
+}
---
+++ simpleAppExpress/server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser');
+
+// app.use(express.static('public'));
+app.use('/static', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Load 'index.html' page
+app.get('/index.html', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+// Get items from the list
+app.get('/get-items', function (req, res) {
+   console.log("Load items from json");
+   res.sendFile( __dirname + "/" + 'input-data.json' );
+})
+
+// Add item to the list
+app.post('/add-item', function (req, res) {
+
+  console.log("Add an element to the list");
+
+  fs.readFile('input-data.json', function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    var newObj = JSON.parse(data);
+    newObj.items.push(req.body.item);
+    console.log(newObj);
+
+    fs.writeFile('input-data.json', JSON.stringify(newObj),  function(err) {
+      if (err) {
+        return console.error(err);
+      }
+      res.sendFile( __dirname + "/" + 'input-data.json' );
+    });
+  });
+})
+
+// Dell items
+
+
+var server = app.listen(4000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
