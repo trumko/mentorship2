f52c1f84a0622a312e2bfe6e7fd0410762aafbcf
Mykhailo Iartysh <mykhailo_iartysh@epam.com>
2016-11-21 15:55:50 +0200

    implemented sockets

--- simpleAppExpress/public/js/script.js
+++ simpleAppExpress/public/js/script.js
@@ -3,122 +3,76 @@ var socket = io();
 var curId = '';
 var isAdd = true;
 
-socket.on('all messages', function(msg){
+socket.on('send all messages', function(msg) {
   console.log(msg);
   $('.all-items').html(generateList(msg))
+  $('.add-input').val('');
 });
 
 // Get all items when page is loaded
 $(function() {
-  $.ajax({
-    method: 'GET',
-    url: '/all-items',
-  })
-    .done(function( msg ) {
-      // console.log(msg);
-      $('.all-items').html(generateList(msg));
-  });
+  socket.emit('show all items', id);
 });
 
 // Check if we add or edit item
 $('.add-item').click(function(event) {
   event.preventDefault();
-  isAdd ? addItem() : editItem()
+  isAdd
+    ? addItem()
+    : editItem()
 })
 
+// Add item to the list
 function addItem() {
   if ($('.add-input').val().trim() == '') {
     return;
   }
-
   socket.emit('add item', $('.add-input').val());
-  $('.add-input').val('');
   return false;
 };
 
-// Edit item from list
-  function editItem() {
-    if (curId == '') {
-      return
-    }
-
-    $.ajax({
-      method: 'PUT',
-      url: '/single-item/' + curId,
-      data: {
-        content: $('.add-input').val(),
-      }
-    })
-      .done( function(msg) {
-        $('.add-input').val('');
-
-        $.ajax({
-          method: 'GET',
-          url: '/all-items',
-        })
-          .done(function( msg ) {
-            console.log(msg);
-            $('.all-items').html(generateList(msg));
-        });
-    });
-  $( ".add-item" ).html('ADD');
-    curId = '';
-    isAdd = true
+// Edit item from the list
+function editItem() {
+  if (curId == '') {
+    return
   }
+
+  socket.emit('edit item', {
+    curId: curId,
+    value: $('.add-input').val()
+  });
+
+  $(".add-item").html('ADD');
+  curId = '';
+  isAdd = true
+}
 
 // Delete all items from list
 $('.del-items').click(function(event) {
   event.preventDefault();
-
-  $.ajax({
-    method: 'DELETE',
-    url: '/all-items',
-  })
-    .done(function(msg) {
-      $.ajax({
-        method: 'GET',
-        url: '/all-items',
-      })
-        .done(function( msg ) {
-          console.log(msg);
-          $('.all-items').html(generateList(msg));
-      });
-  });
+  socket.emit('del all');
+  return false;
 })
 
-function onDelItem(id){
-  $.ajax({
-    method: 'DELETE',
-    url: '/single-item',
-    data: {id: id}
-  })
-    .done(function(msg) {
-      $.ajax({
-        method: 'GET',
-        url: '/all-items',
-      })
-        .done(function( msg ) {
-          console.log(msg);
-          $('.all-items').html(generateList(msg));
-      });
-  });
+function onDelItem(id) {
+  socket.emit('del single item', id);
 }
 
-function onEditItem(id, content){
+function onEditItem(id, content) {
   curId = id;
   isAdd = false;
   $('.add-input').val(content);
-  $( ".add-item" ).html('EDIT');
+  $(".add-item").html('EDIT');
 }
 
 function generateList(itemData) {
-  var itemList = itemData.reduce(function(prevList, current){
+  var itemList = itemData.reduce(function(prevList, current) {
     return (`${prevList}
-            <li>
-              <button onclick="onDelItem('${current._id}')">del</button>
-              <button onclick="onEditItem('${current._id}', '${current.content}')">edit</button>
-              <span>${current.content}</span>
-            </li>`)
+      <li>
+        <button onclick="onDelItem('${current._id}')">del</button>
+        <button onclick="onEditItem('${current._id}', '${current.content}')">edit</button>
+        <span>${current.content}</span>
+      </li>`)
   }, '')
   // console.log(itemList);
   return '<ul>' + itemList + '</ul>'
--- simpleAppExpress/server.js
+++ simpleAppExpress/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
 var http = require('http').Server(app);
+
 var io = require('socket.io')(http);
 
 var mongoose = require('mongoose');
@@ -10,74 +10,83 @@ var bodyParser = require('body-parser');
 var Item = require('./mongooseConf.js');
 
 app.use('/static', express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Load 'index.html' page
-app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.sendFile( __dirname + "/" + "index.html" );
+app.get('/', function(req, res) {
+  console.log("Got a GET request for the homepage");
+  res.sendFile(__dirname + "/" + "index.html");
 })
 
-app.get('/index.html', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.sendFile( __dirname + "/" + "index.html" );
+app.get('/index.html', function(req, res) {
+  console.log("Got a GET request for the homepage");
+  res.sendFile(__dirname + "/" + "index.html");
 })
 
 // Get items from the list
-app.get('/all-items', function (req, res) {
-   console.log("Load items from json");
+io.on('connection', function(socket) {
+  Item.find({}, function(err, items) {
+    console.log(items);
+    io.emit('send all messages', items)
+  })
+});
 
-   Item.find({}, function(err, items) {
-     console.log(items);
-     res.send(items)
-   })
-})
-
-// Add item
-io.on('connection', function(socket){
-  socket.on('add item', function(msg){
+// Add item to the list
+io.on('connection', function(socket) {
+  socket.on('add item', function(msg) {
     var item = new Item({content: msg});
-
-    item.save(function (err, TodoObject) {
+    item.save(function(err, TodoObject) {
       Item.find({}, function(err, items) {
-        io.emit('all messages', items)
+        io.emit('send all messages', items)
       })
-
     });
   });
 });
 
 // Edit item from the list
-app.put('/single-item/:id', function (req, res) {
-  Item.findOne({"_id": req.params.id}, function(err, item){
-    if (err) {
-      return console.error(err);
-    }
-    item.content = req.body.content;
+io.on('connection', function(socket) {
+  socket.on('edit item', function(msg) {
+    console.log(msg.curId, msg.value);
 
-    item.save (function(err, item){
-      res.send(item);
+    Item.findOne({
+      "_id": msg.curId
+    }, function(err, item) {
+      if (err) {
+        return console.error(err);
+      }
+      item.content = msg.value;
+
+      item.save(function(err, TodoObject) {
+        Item.find({}, function(err, items) {
+            io.emit('send all messages', items)
+        })
+      });
     })
-  })
-})
+  });
+});
 
-// Dell items
-app.delete('/all-items', function (req, res) {
-  Item.find({}).remove(function(err, items){
-    res.send('del all')
-   })
-})
+// Dell all items from the list
+io.on('connection', function(socket) {
+  socket.on('del all', function() {
+    Item.find({}).remove(function(err, items) {
+      io.emit('send all messages', [])
+      })
+  });
+});
 
-// Dell item
-app.delete('/single-item', function (req, res) {
+// Dell single item from the list
+io.on('connection', function(socket) {
+  socket.on('del single item', function(id) {
+    Item.find({"_id": id}).remove(function(err, items) {
+      Item.find({}, function(err, items) {
+        io.emit('send all messages', items)
+      })
+    })
+  });
+});
 
-  Item.find({"_id": req.body.id}).remove(function(err, items){
-    res.send('del item')
-   })
-})
-
-var server = http.listen(4000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+var server = http.listen(4000, function() {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
 })
